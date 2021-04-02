18f32352b9c233b89bf4767c2824d8a89a49a1e7
P.J. Finlay <pjappdevelopment@gmail.com>
2021-04-02 09:36:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PyQt Starter
+
---
+++ my_application.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+app = QApplication([])
+window = QMainWindow()
+window.show()
+self.app.exec_()
+
